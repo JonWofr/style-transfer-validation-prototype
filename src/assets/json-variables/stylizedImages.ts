aad571ed8264ca411d2f224572b295de99f3fd4c
Nico Burkart <nb094@hdm-stuttgart.de>
2021-04-12 09:29:26 +0200

    fixed styles swiper

--- src/assets/json-variables/stylizedImages.ts
+++ src/assets/json-variables/stylizedImages.ts
@@ -3,11 +3,62 @@ export const stylizedImagePreviews: {
   appliedStyleImageName: string;
 }[] = [
   {
-    publicUrl: '/assets/images/stylized-image-1.jpg',
-    appliedStyleImageName: 'Composition VII',
+    publicUrl: '/assets/images/preview-images/gray_cat_doggo.jpg',
+    appliedStyleImageName: 'Sprinkle',
   },
   {
-    publicUrl: '/assets/images/stylized-image-2.jpg',
+    publicUrl: '/assets/images/preview-images/gray_cat_watercolor_fox.jpg',
+    appliedStyleImageName: 'Watercolor',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/gray_cat_style-17.jpg',
+    appliedStyleImageName: 'Newspaper',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/gray_cat_nice1.jpg',
+    appliedStyleImageName: 'Blue Monsoon',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/gray_cat_modern-art.jpg',
+    appliedStyleImageName: 'Modern Strokes',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/gray_cat_composition-vii.jpg',
+    appliedStyleImageName: 'Colorful Shapes',
+  },
+  {
+    publicUrl:
+      '/assets/images/preview-images/gray_cat_b600adcf819944739847ea5859ae8d39_opt.jpg',
+    appliedStyleImageName: 'Rusty White',
+  },
+
+  {
+    publicUrl: '/assets/images/preview-images/golden_doggo_2.jpg',
     appliedStyleImageName: 'Sprinkle',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/golden_watercolor_fox.jpg',
+    appliedStyleImageName: 'Watercolor',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/golden_style-17.jpg',
+    appliedStyleImageName: 'Newspaper',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/golden_nice1.jpg',
+    appliedStyleImageName: 'Blue Monsoon',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/golden_modern-art.jpg',
+    appliedStyleImageName: 'Modern Strokes',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/golden_composition-vii.jpg',
+    appliedStyleImageName: 'Colorful Shapes',
+  },
+  {
+    publicUrl:
+      '/assets/images/preview-images/golden_b600adcf819944739847ea5859ae8d39_opt.jpg',
+    appliedStyleImageName: 'Rusty White',
   },
 ];
