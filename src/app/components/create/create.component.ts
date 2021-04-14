f77acf72b6647182043ee4e984aa34fb502e096f
Nico Burkart <nb094@hdm-stuttgart.de>
2021-04-14 07:39:32 +0200

    preview images

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -51,6 +51,7 @@ export class CreateComponent implements OnInit {
   hasCreatedNewDocument = false;
 
   ngOnInit(): void {
+    this.shuffle(this.stylizedImagePreviews);
     this.matchingStylizedImagePreviews = this.stylizedImagePreviews;
   }
 
@@ -153,5 +154,25 @@ export class CreateComponent implements OnInit {
 
   toggleFitStyleView(shouldShow: boolean) {
     this.shouldShowFitStyleView = shouldShow;
+  }
+
+  shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
   }
 }
--- src/assets/json-variables/stylizedImages.ts
+++ src/assets/json-variables/stylizedImages.ts
@@ -61,4 +61,139 @@ export const stylizedImagePreviews: {
       '/assets/images/preview-images/golden_b600adcf819944739847ea5859ae8d39_opt.jpg',
     appliedStyleImageName: 'Rusty White',
   },
+
+  {
+    publicUrl: '/assets/images/preview-images/black_and_white_dog_doggo.jpg',
+    appliedStyleImageName: 'Sprinkle',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/black_and_white_dog_style-17.jpg',
+    appliedStyleImageName: 'Newspaper',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/black_and_white_dog_nice1.jpg',
+    appliedStyleImageName: 'Blue Monsoon',
+  },
+  {
+    publicUrl:
+      '/assets/images/preview-images/black_and_white_dog_modern-art.jpg',
+    appliedStyleImageName: 'Modern Strokes',
+  },
+  {
+    publicUrl:
+      '/assets/images/preview-images/black_and_white_dog_composition-vii.jpg',
+    appliedStyleImageName: 'Colorful Shapes',
+  },
+  {
+    publicUrl:
+      '/assets/images/preview-images/black_and_white_dog_b600adcf819944739847ea5859ae8d39_opt.jpg',
+    appliedStyleImageName: 'Rusty White',
+  },
+
+  {
+    publicUrl: '/assets/images/preview-images/laying_cat_doggo.jpg',
+    appliedStyleImageName: 'Sprinkle',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/laying_cat_style-17.jpg',
+    appliedStyleImageName: 'Newspaper',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/laying_cat_nice1.jpg',
+    appliedStyleImageName: 'Blue Monsoon',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/laying_cat_composition-vii.jpg',
+    appliedStyleImageName: 'Colorful Shapes',
+  },
+
+  {
+    publicUrl: '/assets/images/preview-images/kitty_doggo.jpg',
+    appliedStyleImageName: 'Sprinkle',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/kitty_watercolor_fox.jpg',
+    appliedStyleImageName: 'Watercolor',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/kitty_style-17.jpg',
+    appliedStyleImageName: 'Newspaper',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/kitty_nice1.jpg',
+    appliedStyleImageName: 'Blue Monsoon',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/kitty_modern-art.jpg',
+    appliedStyleImageName: 'Modern Strokes',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/kitty_composition-vii.jpg',
+    appliedStyleImageName: 'Colorful Shapes',
+  },
+  {
+    publicUrl:
+      '/assets/images/preview-images/kitty_b600adcf819944739847ea5859ae8d39_opt.jpg',
+    appliedStyleImageName: 'Rusty White',
+  },
+
+  {
+    publicUrl: '/assets/images/preview-images/dachshund_doggo.jpg',
+    appliedStyleImageName: 'Sprinkle',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/dachshund_watercolor_fox.jpg',
+    appliedStyleImageName: 'Watercolor',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/dachshund_style-17.jpg',
+    appliedStyleImageName: 'Newspaper',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/dachshund_nice1.jpg',
+    appliedStyleImageName: 'Blue Monsoon',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/dachshund_modern-art.jpg',
+    appliedStyleImageName: 'Modern Strokes',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/dachshund_composition-vii.jpg',
+    appliedStyleImageName: 'Colorful Shapes',
+  },
+  {
+    publicUrl:
+      '/assets/images/preview-images/dachshund_b600adcf819944739847ea5859ae8d39_opt.jpg',
+    appliedStyleImageName: 'Rusty White',
+  },
+
+  {
+    publicUrl: '/assets/images/preview-images/pacho_doggo.jpg',
+    appliedStyleImageName: 'Sprinkle',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/pacho_watercolor_fox.jpg',
+    appliedStyleImageName: 'Watercolor',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/pacho_style-17.jpg',
+    appliedStyleImageName: 'Newspaper',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/pacho_nice1.jpg',
+    appliedStyleImageName: 'Blue Monsoon',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/pacho_modern-art.jpg',
+    appliedStyleImageName: 'Modern Strokes',
+  },
+  {
+    publicUrl: '/assets/images/preview-images/pacho_composition-vii.jpg',
+    appliedStyleImageName: 'Colorful Shapes',
+  },
+  {
+    publicUrl:
+      '/assets/images/preview-images/pacho_b600adcf819944739847ea5859ae8d39_opt.jpg',
+    appliedStyleImageName: 'Rusty White',
+  },
 ];
